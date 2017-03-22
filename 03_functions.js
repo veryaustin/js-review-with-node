a710241f0e98cae7176a77c18c2356443c616bfa
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 00:19:06 -0500

    Functions: Simulate dice roll

---
+++ 03_functions.js
@@ -0,0 +1,15 @@
+// --- Functions --- //
+
+var firstDie = 5;
+var secondDie = 3;
+
+// var value = 6 * Math.random();
+// var roll = Math.ceil(value);
+// console.log(roll);
+
+// Refactor of above
+console.log(Math.ceil(6 * Math.random()));
+
+// console.log(firstDie);
+// console.log(secondDie);
+// console.log(firstDie + secondDie);
