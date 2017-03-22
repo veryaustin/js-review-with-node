5e3ffbab826f2320e74b56c56cb8bbf5dc1fd546
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 00:23:06 -0500

    Making the initial three console.log statements work

--- 03_functions.js
+++ 03_functions.js
@@ -1,15 +1,15 @@
 // --- Functions --- //
 
-var firstDie = 5;
-var secondDie = 3;
+var firstDie = Math.ceil(6 * Math.random());
+var secondDie = Math.ceil(6 * Math.random());
 
 // var value = 6 * Math.random();
 // var roll = Math.ceil(value);
 // console.log(roll);
 
 // Refactor of above
-console.log(Math.ceil(6 * Math.random()));
+// console.log(Math.ceil(6 * Math.random()));
 
-// console.log(firstDie);
-// console.log(secondDie);
-// console.log(firstDie + secondDie);
+console.log(firstDie);
+console.log(secondDie);
+console.log(firstDie + secondDie);
