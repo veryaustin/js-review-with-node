0ac9024cb09fe449ebdc90e8749e8242cb1a2ec2
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 11:46:15 -0500

    Node/JS Variables

---
+++ 02_variables.js
@@ -0,0 +1,19 @@
+// var x;
+// console.log(x);
+// Without var x, it wouldr return not defined as the variable x isn't defined. 
+// With var x set, it returns as undefined as it sees there is a variable x but nothing is assigned to it
+
+// Define variables in one line
+// var x, y = 2;
+
+x = 3;
+console.log(x);
+// 3
+
+// Re-assign the value of x
+x = 5
+console.log(x);
+// 5
+
+console.log(y);
+// 2
