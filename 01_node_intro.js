9c1a8fcf5b1ce8df65ac69e81694fe122ce4a9d0
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 11:18:49 -0500

    Intro to node REPL

---
+++ 01_node_intro.js
@@ -0,0 +1,27 @@
+// Assignment
+// Node does not require var or const to assign a variable in the REPL
+
+// Assignment
+lunch = 'pizza'
+
+// Re-Assignment
+lunch = 'fish and chips'
+
+// Strings & Numbers
+lunch = '12 inch pizza'
+sizeOfParty = 4
+totalCost = 14.99
+
+// Math Operations
+totalCost / sizeOfParty // 3.7475
+
+// Assignment of math operations to variable
+averageCost = totalCost / sizeOfParty
+
+averageCost // 3.7475
+
+// Message
+message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCost
+
+message
+// message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCosts
