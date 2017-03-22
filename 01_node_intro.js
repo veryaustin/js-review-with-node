e6f2b602e0532f07d7f6923070c429d528ff45ba
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 11:23:07 -0500

    Intro example without REPL

--- 01_node_intro.js
+++ 01_node_intro.js
@@ -1,5 +1,5 @@
-// Assignment
-// Node does not require var or const to assign a variable in the REPL
+//--- Variable Assignment REPL ---//
+// Node does not require var to assign a variable in the REPL
 
 // Assignment
 lunch = 'pizza'
@@ -24,4 +24,35 @@ averageCost // 3.7475
 message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCost
 
 message
+// message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCosts
+
+
+
+
+//--- Variable Assignment In JS File ---//
+// As with all JS files, variable assignment requires var
+
+// Variable Assignment
+var lunch = 'pizza';
+
+// Re-Assignment
+lunch = 'fish and chips';
+
+// Strings & Numbers
+lunch = '12 inch pizza';
+var vsizeOfParty = 4;
+var totalCost = 14.99;
+
+// Math Operations
+console.log(totalCost / sizeOfParty); // 3.7475
+
+// Assignment of math operations to variable
+var averageCost = totalCost / sizeOfParty;
+
+console.log(averageCost); // 3.7475
+
+// Message
+var message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCost;
+
+console.log(message);
 // message = 'Your lunch of ' + lunch + ' for ' + sizeOfParty + ' people will cost you ' + averageCosts
