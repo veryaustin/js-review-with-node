e310c3259627d2659d0ca85d74d44ff3fa049149
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 00:34:07 -0500

    Refactor Dice roll action into function

--- 03_functions.js
+++ 03_functions.js
@@ -1,7 +1,7 @@
 // --- Functions --- //
 
-var firstDie = Math.ceil(6 * Math.random());
-var secondDie = Math.ceil(6 * Math.random());
+// var firstDie = Math.ceil(6 * Math.random());
+// var secondDie = Math.ceil(6 * Math.random());
 
 // var value = 6 * Math.random();
 // var roll = Math.ceil(value);
@@ -10,6 +10,22 @@ var secondDie = Math.ceil(6 * Math.random());
 // Refactor of above
 // console.log(Math.ceil(6 * Math.random()));
 
+// console.log(firstDie);
+// console.log(secondDie);
+// console.log(firstDie + secondDie);
+
+
+
+// --- Refactor Die Roll into function --- //
+var getDieRoll = function () {
+  console.log("Rolling a die");
+  return Math.ceil(6 * Math.random());
+};
+
+var firstDie = getDieRoll();
 console.log(firstDie);
+
+var secondDie = getDieRoll();
 console.log(secondDie);
+
 console.log(firstDie + secondDie);
