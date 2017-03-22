a35f853e382552de2435c8be5f311ef7e889c69d
Austin Lauritsen <austin@veryaustin.com>
2017-03-22 00:39:15 -0500

    Die Roll accepts different sized dice

--- 03_functions.js
+++ 03_functions.js
@@ -17,15 +17,31 @@
 
 
 // --- Refactor Die Roll into function --- //
-var getDieRoll = function () {
-  console.log("Rolling a die");
-  return Math.ceil(6 * Math.random());
+
+// var getDieRoll = function() {
+//   console.log("Rolling a die");
+//   return Math.ceil(6 * Math.random());
+// };
+
+// var firstDie = getDieRoll();
+// console.log(firstDie);
+
+// var secondDie = getDieRoll();
+// console.log(secondDie);
+
+// console.log(firstDie + secondDie);
+
+
+
+// --- Refactor Die Roll acceps different sized dice --- //
+
+var getDieRoll = function (dieSize) {
+  console.log(dieSize);
+  return Math.ceil(dieSize * Math.random());
 };
 
-var firstDie = getDieRoll();
+var firstDie = getDieRoll(10);
+var secondDie = getDieRoll(10);
 console.log(firstDie);
-
-var secondDie = getDieRoll();
 console.log(secondDie);
-
 console.log(firstDie + secondDie);
